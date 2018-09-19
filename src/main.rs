67a441a702bf9a30206f5218915da3c7be842ef5
Jonas Erthal <erthaljonas@gmail.com>
2018-09-19 16:24:04 -0400

    Added Random Crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
     println!("Please guess a number");
 
     let mut guess = String::new();
@@ -9,5 +16,13 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
     
-    println!("You guessed: {}", guess);
+    println!("Thanks! You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+
 }
